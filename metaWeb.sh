09ca7adf0494b89216eab7310f182e2edc71513c
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2025-02-07 15:45:53 +0100

    Update metaWeb.sh

--- metaWeb.sh
+++ metaWeb.sh
@@ -33,7 +33,7 @@ BLANC='\033[1;37m'
 
 read -p "Entrez une IP scanner: " IP
 read -p "Entrez le nom du scan: " name
- echo " "
+echo " "
  
 # Cree le dossier de scan
 mkdir -p ${name}/nuclei ${name}/nikto ${name}/wapiti ${name}/skipfish ${name}/ZAP $HOME/metaWeb
@@ -41,42 +41,42 @@ mkdir -p ${name}/nuclei ${name}/nikto ${name}/wapiti ${name}/skipfish ${name}/ZA
 # Nuclei
 nuclei_cmd() {
 
-    echo -n "${VERT}[+]${RESET} ${BLANC}Scan Nuclei${RESET} ${VERT}${IP}${RESET}..."
+    echo -ne "${VERT}[+]${RESET} ${BLANC}Scan Nuclei${RESET} ${VERT}${IP}${RESET}..."
     nuclei -u http://${IP} -o ${name}/nuclei/${name}-rapport.txt > /dev/null 2>&1
-    echo "${BLEU}termniner${RESET}"
+    echo -e "${BLEU}termniner${RESET}"
 }
 
 # Nikto
 nikto_cmd() {
 
-    echo -n "${VERT}[+]${RESET} ${BLANC}Scan Nikto${RESET} ${VERT}${IP}${RESET}..."
+    echo -ne "${VERT}[+]${RESET} ${BLANC}Scan Nikto${RESET} ${VERT}${IP}${RESET}..."
     nikto -url http://${IP} -C all -Format htm -o ${name}/nikto/${name}-rapport.html > /dev/null 2>&1
-    echo "${BLEU}termniner${RESET}"
+    echo -e "${BLEU}termniner${RESET}"
 }
 
 # Wapiti
 wapiti_cmd() {
 
-    echo -n "${VERT}[+]${RESET} ${BLANC}Scan Wapiti${RESET} ${VERT}${IP}${RESET}..."
+    echo -ne "${VERT}[+]${RESET} ${BLANC}Scan Wapiti${RESET} ${VERT}${IP}${RESET}..."
     wapiti -u http://${IP} --flush-session -o ${name}/wapiti/${name}-rapport.txt --format txt > /dev/null 2>&1
-    echo "${BLEU}termniner${RESET}"
+    echo -n "${BLEU}termniner${RESET}"
 }
 
 # Skipfish
 skipfish_cmd() {
 
-    echo -n "${VERT}[+]${RESET} ${BLANC}Scan Skipfish${RESET} ${VERT}${IP}${RESET}..."
+    echo -ne "${VERT}[+]${RESET} ${BLANC}Scan Skipfish${RESET} ${VERT}${IP}${RESET}..."
     skipfish -o ${name}/skipfish http://${IP} > /dev/null 2>&1
-    echo "${BLEU}termniner${RESET}"
+    echo -e "${BLEU}termniner${RESET}"
 }
 
 # ZAP-OWASP a ajuster 
 ZAP_cmd() {
 
-    echo -n "${VERT}[+]${RESET} ${BLANC}Scan ZAP${RESET} ${VERT}${IP}${RESET}..."
+    echo -ne "${VERT}[+]${RESET} ${BLANC}Scan ZAP${RESET} ${VERT}${IP}${RESET}..."
     sh ZAP_2.16.0/zap.sh -cmd -quickurl http://${IP} -quickout $HOME/${name}/ZAP/${name}-rapport.html -quickprogress > /dev/null 2>&1
     #zap.sh -cmd -quickurl http://${IP} -quickout $HOME/Documents/${name}/ZAP/${name}-rapport.html -quickprogress
-    echo "${BLEU}termniner${RESET}"
+    echo -e "${BLEU}termniner${RESET}"
 }
 
 # Deplace les rapports
